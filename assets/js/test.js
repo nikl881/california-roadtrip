5de1d7c812f7d0e7b39f943b3ba29bd7d71f735a
Niels <deklerkn@xs4all.nl>
2019-02-06 12:49:48 +0000

    added JS functionality to POI buttons

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,5 +1,28 @@
-function picture(){ 
+function picture1(){ 
         var pic = "assets/css/images/demo_img.jpeg"
-        document.getElementById('bigpic').src = pic.replace('90x90', '225x225');
+        document.getElementById('bigpic1' ).src = pic.replace('70x90', '225x225');
 
         }
+        
+
+function picture2(){ 
+        var pic = "assets/css/images/demo_img2.jpg"
+        document.getElementById('bigpic2').src = pic.replace('90x90', '225x225');
+
+        }
+        
+        
+function picture3(){ 
+        var pic = "assets/css/images/demo_img3.jpg"
+        document.getElementById('bigpic3').src = pic.replace('90x90', '225x225');
+
+        }
+        
+
+
+function button2(){ 
+        var pic = "assets/css/images/demo_img.jpeg"
+        document.getElementById('button2' ).src = pic.replace('90x90', '225x225');
+
+        }
+        
