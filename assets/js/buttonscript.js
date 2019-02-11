608c1a7564ea38980169a64ddb8da2bc3d65c35a
Niels <deklerkn@xs4all.nl>
2019-02-11 16:11:47 +0000

    removed unnecessary code

---
+++ assets/js/buttonscript.js
@@ -0,0 +1,34 @@
+
+    $('#buttontest').on("click",function(e)
+    {  
+    $('#LogoTest').toggle();
+    });
+    $('#buttontest').on("click",function(e)
+    {  
+    $('h5').toggle();
+    
+    }); 
+    
+    $('#buttontest-2').on("click",function(e)
+    {  
+    $('#LogoTest-2').toggle();
+    
+    });
+    
+    $('#buttontest-2').on("click",function(e)
+    {  
+    $('h6').toggle();
+    
+    }); 
+    
+        $('#buttontest-3').on("click",function(e)
+    {  
+    $('#LogoTest-3').toggle();
+    
+    });
+    
+    $('#buttontest-3').on("click",function(e)
+    {  
+    $('h7').toggle();
+    
+    }); 
