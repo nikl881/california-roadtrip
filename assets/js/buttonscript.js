a76fea654a8250f4ec7787c140676fd23f4d72a9
Niels <deklerkn@xs4all.nl>
2019-02-12 15:41:27 +0000

    fix jquery function on accom.buttons

--- assets/js/buttonscript.js
+++ assets/js/buttonscript.js
@@ -7,21 +7,37 @@
     {  
     $('h4').toggle();
     
+    
+    $(document).ready(function(){
+    $( "#buttontest" ).click(function(){
+    $( "#inn-preview" ).hide();
+    $( "#inn-preview" ).show();
+    });   
+    });
     }); 
     
     $('#buttontest-2').on("click",function(e)
     {  
+        
     $('#LogoTest-2').toggle();
-    
     });
     
     $('#buttontest-2').on("click",function(e)
     {  
+        
     $('h5').toggle();
     
+    $(document).ready(function(){
+    $( "#buttontest-2" ).click(function(){
+
+    $( "#inn-preview" ).hide();
+    $( "#inn-preview" ).show();
+    
+    });  
+    });
     }); 
     
-        $('#buttontest-3').on("click",function(e)
+    $('#buttontest-3').on("click",function(e)
     {  
     $('#LogoTest-3').toggle();
     
@@ -31,4 +47,11 @@
     {  
     $('h6').toggle();
     
+    $(document).ready(function(){
+    $( "#buttontest-3" ).click(function(){
+    $( "#inn-preview" ).hide();
+    $( "#inn-preview" ).show();
+    
+    });   
+    });
     }); 
